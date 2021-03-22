84cd2359cffdbd73ee7b90888ee7dd6a196f669d
Mateusz Derks <mateusz.derks@toptal.com>
2021-03-22 09:19:52 +0100

    Initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/demo', '<rootDir>/libs/mylib'],
+  projects: [
+    '<rootDir>/apps/demo',
+    '<rootDir>/libs/mylib',
+    '<rootDir>/apps/api',
+  ],
 };
